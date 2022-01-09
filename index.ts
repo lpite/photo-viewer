912bd731ce39f86765b1819140eb7ac39637770a
lpite <farionsasa@gmail.com>
2022-01-09 14:54:07 +0200

    Update index.ts

--- index.ts
+++ index.ts
@@ -10,7 +10,7 @@ app.use(cors());
 
 app.get("/img", (req, res) => {
   const files = fs.readdirSync("../storage/dcim/Camera");
-  res.send({ images: files });
+  res.send({ images: files.reverse() });
 });
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "./index.html"));
