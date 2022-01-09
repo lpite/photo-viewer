129fc5220451ef2026e8aec48e8d9343fff0d80b
lpite <farionsasa@gmail.com>
2022-01-09 20:51:04 +0200

    ts-node

--- index.ts
+++ index.ts
@@ -9,10 +9,14 @@ app.use(express.static("../storage/dcim/Camera"));
 app.use(cors());
 
 app.get("/img", (req, res) => {
+  console.log("images");
+
   const files = fs.readdirSync("../storage/dcim/Camera");
   res.send({ images: files.reverse() });
 });
 app.get("/", (req, res) => {
+  console.log("html");
+
   res.sendFile(path.join(__dirname, "./index.html"));
 });
 
