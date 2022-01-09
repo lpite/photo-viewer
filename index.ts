60989d5b211209aba9fb3888564514e0986f5f83
lpite <farionsasa@gmail.com>
2022-01-09 14:40:55 +0200

    first

---
+++ index.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import path from "path";
+import fs from "fs";
+const app = express();
+
+app.use(express.static("."));
+app.use(express.static("./Camera"));
+
+app.get("/img", (req, res) => {
+  res.send({ images: ["1.png"] });
+});
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./index.html"));
+});
+
+app.listen(3000, () => {
+  console.log("startet");
+});
---
+++ script.js
@@ -0,0 +1,14 @@
+const body = document.getElementsByTagName("body")[0];
+async function main() {
+  await fetch("http://localhost:3000/img").then((res) => {
+    const json = res.json().then((re) => {
+      console.log(re);
+      re.images.forEach((img) => {
+        const image = document.createElement("img");
+        image.src = img;
+        body.appendChild(image);
+      });
+    });
+  });
+}
+main();
